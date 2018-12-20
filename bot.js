ea0bdd2d6ca7003e104e950ceda4e8fa68b37aff
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-20 23:46:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,23 @@ client.on('message', async message => {
 
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+  if (command === "Gsay") { /// اكتب البرفيكس ثم الامر
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+
+
+});
 
 
 
